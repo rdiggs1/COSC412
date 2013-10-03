34366434b9b403c82fb8b480b5843c08f7249738
Ronald Diggs <rdiggs1@students.towson.edu>
2013-10-03 02:58:36 -0400

    test
    testing

---
+++ HelpActivity.java
@@ -0,0 +1,17 @@
+package com.example.basictodolist;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class HelpActivity extends MainActivity 
+{
+	Button button;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.help);
+	}
+}
